ddb06a5c9c91ee08493885837e5b071579b3a3a3
Iury da Rocha Miguel <iury.miguel91@gmail.com>
2022-06-20 20:06:31 -0300

    Pod install and .gitignore

---
+++ SyncData/Pods/Target Support Files/Pods-SyncData-SyncDataUITests/Pods-SyncData-SyncDataUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SyncData_SyncDataUITests : NSObject
+@end
+@implementation PodsDummy_Pods_SyncData_SyncDataUITests
+@end
---
+++ SyncData/Pods/Target Support Files/Pods-SyncDataTests/Pods-SyncDataTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SyncDataTests : NSObject
+@end
+@implementation PodsDummy_Pods_SyncDataTests
+@end
